7874c2a4a382b8f0737c755279f56e445a6b28b2
= <=>
2022-07-31 21:13:46 +0530

    third  commit

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,7 @@ import Navbar from './componants/pages/Navbar';
  import Home from './componants/pages/Home';
  import Register from './componants/pages/Register';
  import Login from './componants/pages/Login';
-import Uploadfile from './componants/pages/Uploadfile';
 import Viewfile from './componants/pages/Viewfile';
-import Download from './componants/pages/Download';
 import Nofile from './componants/pages/Nofile';
 
 
@@ -25,9 +23,7 @@ function App() {
             <Route exact path="/" element={<Login />} />
             <Route exact path="/register" element={<Register />} />
             <Route exact path="/home" element={<Home />} />
-            {/* <Route exact path="/upload" element={<Uploadfile />} /> */}
             <Route exact path="/viewfile" element={<Viewfile />} />
-            <Route exact path="/download" element={< Download/>} />
             <Route exact path="/nofile" element={< Nofile/>} />
             
 
--- client/src/componants/pages/Viewfile.js
+++ client/src/componants/pages/Viewfile.js
@@ -10,10 +10,16 @@ function Viewfile() {
     let navigate = useNavigate();
     const [Data, setData] = useState([])
     const [key, setkey] = useState({ "key": "" })
-    const [compare_key, setcompare_key]=useState("")
+    const [id, setId] = useState("")
+    const [path, setPath] = useState("")
+    const [mimetype,setmimetype] = useState("")
+    const [compare_key, setcompare_key] = useState("")
+    
+
+//////////model////////////
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
-    // const handleShow = () => setShow(true);
+   
 
 
 
@@ -46,6 +52,7 @@ function Viewfile() {
         fetch_files()
 
     }, [])
+
     ///////////delete the data///////
     const deleteData = (id) => {
         const config = {
@@ -83,6 +90,12 @@ function Viewfile() {
 
     }, [])
 
+
+
+
+
+    /////////download data/////////
+
     const downloadData = (id, path, mimetype) => {
         axios.get(
             "http://localhost:5000/api/downloadFile/" + id,
@@ -97,50 +110,57 @@ function Viewfile() {
             console.log(filename);
             console.log(mimetype1);
             return download(data.data, filename, mimetype);
+             
         }).catch((err) => {
             console.log(err);
         });
     };
 
     ///////model download file/////
-    const modeldata=(id, path, mimetype,security_key)=>{
-        var id=id;
-        var path=path;
-        var mimetype=mimetype;
+    const modeldata = (id, path, mimetype, security_key) => {
+        var id = id;
+        var path = path;
+        var mimetype = mimetype;
+        setId(id)
+        setPath(path)
+        setmimetype(mimetype)
         setcompare_key(security_key)
         setShow(true)
     }
-
-    const downloadFile =()=>{
-        if(key===compare_key){
-            downloadData();
-            console.log("downloadFile")
+   
+    ///////compare key////////
+    const downloadFile = () => {
+        if (key === compare_key) {
+         downloadData(id, path, mimetype);
+         console.log("downloadFile")
+        }else{
+            window.alert("Incorrect key, please enter correct key")
         }
-         
+
     }
 
 
     return (
         <div className="container-fluid">
             <div>
-       <Modal show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                  <Modal.Title>Downlod file</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                   <form className='mt-3'>
-                      <label>please enter the key</label>
-                      <input  onChange={(e)=>setkey(e.target.value)} type='text' className='form-control' 
-                       />
-                   </form>
-                </Modal.Body>
-                <Modal.Footer>
-                   <Button variant="secondary" onClick={handleClose}>Close</Button>
-                   <button  className='btn btn-primary' onClick={downloadFile} >Download Now</button>
-                </Modal.Footer>
-             </Modal>
-       </div>
-        
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Downlod file</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <form className='mt-3'>
+                            <label>please enter the key</label>
+                            <input onChange={(e) => setkey(e.target.value)} type='text' className='form-control'
+                            />
+                        </form>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>Close</Button>
+                        <button className='btn btn-primary' onClick={downloadFile} >Download Now</button>
+                    </Modal.Footer>
+                </Modal>
+            </div>
+
             <div>
                 <Link className='mt-3 my-5' to="/home">Back to the homepage</Link>
             </div>
@@ -170,8 +190,6 @@ function Viewfile() {
                                 {
                                     Data.map((ele, ind) => {
                                         return <tr key={ele.id} >
-                                            {/* <th scope="row"></th> */}
-                                            {/* <td>{ele._id}</td> */}
                                             <td>{ele.fileName}</td>
                                             <td>{ele.filePath}</td>
                                             <td>{ele.fileType}</td>
@@ -183,7 +201,7 @@ function Viewfile() {
                                             </td>
                                             <td>
                                                 {/* <button className='btn btn-sm mx-3'type="download" onClick={()=>downloadData(ele._id, ele.filePath, ele.fileType)} >Download</button> */}
-                                                <button className='btn btn-sm mx-3' variant="primary" onClick={()=>modeldata(ele._id, ele.filePath, ele.fileType, ele.key)} >Download</button>
+                                                <button className='btn btn-sm mx-3' variant="primary" onClick={() => modeldata(ele._id, ele.filePath, ele.fileType, ele.key)} >Download</button>
                                             </td>
 
                                         </tr>
